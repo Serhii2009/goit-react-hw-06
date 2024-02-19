9872185984c1615fce3d6e764bb3316d6f5b32e3
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-02-19 14:01:42 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,21 +1,71 @@
+// import { useId } from "react";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+// import * as Yup from "yup";
+// import css from "./ContactForm.module.css";
+// import { useDispatch } from "react-redux";
+// import { addContact } from "../../reduxe/contactSlice";
+
+// const userSchema = Yup.object().shape({
+//   name: Yup.string()
+//     .min(3, "Name must be at least 3 symb long")
+//     .required("This is a required field"),
+//   number: Yup.string()
+//     .matches(
+//       /^\d{3}-\d{2}-\d{2}$/,
+//       "Phone number must have only numbers and in xxx-xx-xx format"
+//     )
+//     .required("This is a required field"),
+// });
+
+// export const ContactForm = () => {
+//   const dispatch = useDispatch();
+
+//   const handleSubmit = (values, actions) => {
+//     const newContact = {
+//       id: values.id,
+//       name: values.name,
+//       number: values.number,
+//     };
+
+//     dispatch(addContact(newContact));
+//     actions.resetForm();
+//   };
+//   const initialValues = { id: "", name: "", number: "" };
+//   const nameFieldId = useId();
+//   const numberFieldId = useId();
+
+//   return (
+//     <Formik
+//       initialValues={initialValues}
+//       onSubmit={handleSubmit}
+//       validationSchema={userSchema}
+//     >
+//       <Form className="css.loginBox" autoComplete="off">
+//         <div className={css.userBox}>
+//           <label htmlFor={nameFieldId}>Name:</label>
+//           <Field type="text" name="name" id={nameFieldId} />
+//           <ErrorMessage name="name" component="span" />
+//         </div>
+
+//         <div className={css.userBox}>
+//           <label htmlFor={numberFieldId}>Number:</label>
+//           <Field type="text" name="number" id={numberFieldId} />
+//           <ErrorMessage name="number" component="span" />
+//         </div>
+
+//         <button className={css.userButton} type="submit">
+//           Add user
+//         </button>
+//       </Form>
+//     </Formik>
+//   );
+// };
+
 import { useId } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import css from "./ContactForm.module.css";
 import { useDispatch } from "react-redux";
 import { addContact } from "../../reduxe/contactSlice";
-
-const userSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(3, "Name must be at least 3 symb long")
-    .required("This is a required field"),
-  number: Yup.string()
-    .matches(
-      /^\d{3}-\d{2}-\d{2}$/,
-      "Phone number must have only numbers and in xxx-xx-xx format"
-    )
-    .required("This is a required field"),
-});
 
 export const ContactForm = () => {
   const dispatch = useDispatch();
@@ -34,28 +84,37 @@ export const ContactForm = () => {
   const nameFieldId = useId();
   const numberFieldId = useId();
 
+  const userSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Name must be at least 3 symb long")
+      .required("This is a required field"),
+    number: Yup.string()
+      .matches(
+        /^\d{3}-\d{2}-\d{2}$/,
+        "Phone number must have only numbers and in xxx-xx-xx format"
+      )
+      .required("This is a required field"),
+  });
+
   return (
     <Formik
       initialValues={initialValues}
       onSubmit={handleSubmit}
       validationSchema={userSchema}
     >
-      <Form className="css.loginBox" autoComplete="off">
-        <div className={css.userBox}>
-          <label htmlFor={nameFieldId}>Name:</label>
-          <Field type="text" name="name" id={nameFieldId} />
+      <Form>
+        <div className="inputBox">
+          <label htmlFor={nameFieldId}>Name</label>
+          <Field id={nameFieldId} type="text" name="name" />
           <ErrorMessage name="name" component="span" />
         </div>
 
-        <div className={css.userBox}>
-          <label htmlFor={numberFieldId}>Number:</label>
-          <Field type="text" name="number" id={numberFieldId} />
+        <div className="inputBox">
+          <label htmlFor={numberFieldId}>Number</label>
+          <Field id={numberFieldId} type="text" name="number" />
           <ErrorMessage name="number" component="span" />
         </div>
-
-        <button className={css.userButton} type="submit">
-          Add user
-        </button>
+        <button type="submit">Add contact</button>
       </Form>
     </Formik>
   );
